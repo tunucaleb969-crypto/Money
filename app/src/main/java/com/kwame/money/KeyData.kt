23d7dad3b531e7592f31b7072836d56b999a124b
tunucaleb969-crypto <tunucaleb969@gmail.com>
2026-09-17 04:30:54 +0000

    Phase 2: add KeyData model

---
+++ app/src/main/java/com/kwame/money/KeyData.kt
@@ -0,0 +1,16 @@
+package com.kwame.money
+
+data class KeyData(
+    val label: String,
+    val capsLabel: String = label.uppercase(),
+    val keyType: KeyType = KeyType.CHARACTER
+)
+
+enum class KeyType {
+    CHARACTER,
+    SHIFT,
+    BACKSPACE,
+    SPACE,
+    ENTER,
+    SYMBOLS
+}
