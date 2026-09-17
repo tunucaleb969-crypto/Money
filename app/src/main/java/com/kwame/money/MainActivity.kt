2bb4b7d332512b32028b7b4bb36c7dd8c6195de9
tunucaleb969-crypto <tunucaleb969@gmail.com>
2026-09-17 11:24:16 +0000

    Switch MainActivity back to AppCompatActivity now that Theme.Money supports it

--- app/src/main/java/com/kwame/money/MainActivity.kt
+++ app/src/main/java/com/kwame/money/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.kwame.money
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
