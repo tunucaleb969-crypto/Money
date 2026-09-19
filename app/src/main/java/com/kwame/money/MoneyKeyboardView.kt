e83e8f9df49c29fe90cbacf2134a8334a0b9f1c7
tunucaleb969-crypto <tunucaleb969@gmail.com>
2026-09-19 04:21:58 +0000

    Fix build: add missing getValue import for State<T> 'by' delegation

--- app/src/main/java/com/kwame/money/MoneyKeyboardView.kt
+++ app/src/main/java/com/kwame/money/MoneyKeyboardView.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
